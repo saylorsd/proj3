15ea9b277c8bc37ec3e16495a987920e1f84e711
saylorsd <Hambone#25>
2016-03-26 18:02:34 -0400

    implemented NRU and clock

---
+++ PageTable.py
@@ -0,0 +1,162 @@
+from copy import deepcopy
+from itertools import cycle
+
+
+class PageTableEntry(object):
+    def __init__(self, page, mode):
+        self.page = page
+
+        self.frame = None
+
+        self.valid = True
+        self.ref = True
+        self.dirty = True if 'W' in mode else False
+
+    def refresh(self):
+        self.ref = False
+
+    def reset(self):
+        ''' Reset to default values after eviction
+        '''
+        self.frame = None
+        self.valid = False
+        self.ref = False
+        self.dirty = False
+
+    def refer(self, mode):
+        self.ref = True
+        if 'W' in mode:
+            self.dirty = True
+
+class PageTable(object):
+    def __init__(self, pages, frames, pr_method):
+        self.size = pages
+        self.frames = frames
+        self.page_table = [None] * pages
+        self.frame_table = [None] * frames
+        self.empty_frames = frames
+
+        # Performance measures
+        self.hits = 0
+        self.faults = 0
+        self.disk_writes = 0
+
+        self.pr_method = pr_method  # method of page-replacemnt to use during evictions
+
+        # Replacement-specific data structures
+        self.fifo = [None for x in range(frames)]
+        self.clock_ptr = 0
+
+    def load_page(self, page, mode):
+        ''' Takes page # from virtual address and loads it into physical memory
+        '''
+        # check if it's already loaded
+        if self.search_frame_table(page):
+            self.page_table[page].refer(mode)
+            self.hits += 1
+            return
+
+        # if no space in RAM, we need to use a page replacement algorithm to pick what to evict
+        elif self.empty_frames == 0:
+            evicted = self.evict()  # page to be evicted
+            cleared_frame = evicted.frame
+            evicted.reset()
+        else:
+            cleared_frame = self.get_empty_frame()
+            self.empty_frames -= 1
+
+        self.faults += 1
+
+        # Create PTE for current attemp at loading address
+        temp_page = PageTableEntry(page, mode)
+        temp_page.frame = cleared_frame
+        temp_page.valid = True
+        temp_page.ref = True
+
+        # add page to page and frame tables
+        self.page_table[page] = deepcopy(temp_page)
+        self.frame_table[cleared_frame] = self.page_table[page]
+
+        # if using clock, add this new page to where hand is pointing
+        # if there wasn't a free frame, evict() will have moved ptr to frame to evict
+        if 'clock' in self.pr_method:
+            self.fifo[self.clock_ptr] = self.page_table[page]
+            self.clock_ptr = (self.clock_ptr + 1) % self.frames  # move clock ptr ahead
+
+
+
+    def evict(self):
+        ''' Choose which frame needs to be evicted when a page fault occurs
+        '''
+        evicted_frame = getattr(self, self.pr_method)()
+        if evicted_frame.dirty:
+            self.disk_writes += 1
+        return evicted_frame
+
+    def get_empty_frame(self):
+        ''' Cycles through frame table and finds the next free frame
+        '''
+        for frame in range(len(self.frame_table)):
+            if self.frame_table[frame] is None:
+                return frame
+
+        return -1
+
+    def search_frame_table(self, page):
+        pte = self.page_table[page]
+        if pte is None:
+            return False
+        else:
+            return pte.frame is not None
+
+
+        return self.page_table[page].frame is not None
+
+    def refresh(self):
+        ''' Refreshes all loaded pages
+        '''
+        for frame in self.frame_table:
+            try:
+                frame.refresh()
+            except:
+                continue
+
+    def is_valid(self, page):
+        ''' Checks validity of page
+        '''
+        return self.page_table[page].valid
+
+    def get_frame(self, page):
+        ''' Gets frame number for page
+        '''
+        return self.page_table[page].frame
+
+    '''
+    PAGE REPLACEMENT ALGORITHMS
+    '''
+
+    def nru(self):
+        nru_classes = [[] for _ in range(4)]
+        # categorize loaded frames
+        for frame in self.frame_table:
+            ref = 2 if frame.ref else 0
+            dirty = 1 if frame.dirty else 0
+            i = ref + dirty
+            nru_classes[i].append(frame)
+
+        # cycle through classes from first choice to last choice until a frame is found
+        for nru_class in nru_classes:
+            if len(nru_class):
+                return nru_class[0]
+
+    def clock(self):
+        i = self.clock_ptr
+        while(1):
+            if not self.fifo[i].ref:
+                self.clock_ptr = i
+                return self.fifo[i]
+            else:
+                self.fifo[i].ref = False
+                i = (i+1) % self.frames
+
+    def aging
--- vmsim.py
+++ vmsim.py
@@ -1,15 +1,18 @@
+from PageTable import PageTable, PageTableEntry
+
 # Constants
-MAX_FRAMES = 10
+MAX_FRAMES = 8
 ADDR_SIZE = 2 ** 32
 PAGE_SIZE = 4 * (2 ** 10)
 MAX_PAGES = ADDR_SIZE // PAGE_SIZE
-hits = 0
-faults = 0
 
 # Initialize stuff
-trace_file = 'bzip.trace'
-page_table = [None] * MAX_PAGES
+trace_file = 'gcc.trace'
+refresh_rate = 1000
+
 frame_table = [None] * MAX_FRAMES
+
+
 
 def parse_trace(line):
     addr, mode = line.rstrip('\n').split(' ')
@@ -17,87 +20,28 @@ def parse_trace(line):
     return addr, mode
 
 
-def split_virtual_address(addr):
-    page_num = addr // PAGE_SIZE
-    offset = addr % PAGE_SIZE
-    return page_num, offset
 
-
-def map_address(addr, mode):
-    '''
-    Adds address
-
-    returns:
-        hit, frame  (if it's a hit, and frame number)
-    '''
-    page_num, offset = split_virtual_address(addr)
-    # check page table for page's PTE
-
-    # if it's not loaded into a frame
-    if page_table[page_num] is None:
-        # make new PTE for it
-        page_table[page_num] = {
-            'frame': get_frame(),
-            'dirty': True if 'w' in mode else False,
-            'ref': True,
-            'valid': True,
-            'page' : page_num,
-            'offset': offset,
-            'addr': addr
-        }
-        hit = False
-    # else, it's already loaded into the frame
-    else:
-        hit = True
-        hits += 1
-
-    # copy pte to frame list
-    frame_table[page_table[page_num]['frame']] = page_table[page_num]
-
-    return hit, page_table[page_num]['frame']
-
-
-def get_frame():
-    # cycle through frames
-    reffed = []
-    unreffed = []
-    for i in range(len(frame_table)):
-        if frame_table[i] is None:
-            return i
-        elif frame_table[i]['ref']:
-            reffed.append(i)
-        else:
-            unreffed.append(i)
-    if len(unreffed)>0:
-        return unreffed[0]
-    else :
-        return reffed[0]
-
-
-def evict():
-    pass
-
+replacement = 'clock'
+pt = PageTable(MAX_PAGES, MAX_FRAMES, replacement)
+traces = 0
 
 with open('traces/' + trace_file) as f:
     for line in (f):
-        # get address
+        traces += 1
+
+        if  traces % refresh_rate == 0:
+            print '--{} Traces Complete --'.format(traces)
+            if replacement == 'NRU':
+                pt.refresh()
+
         addr, mode = parse_trace(line)
+        page = addr // PAGE_SIZE
+        pt.load_page(page, mode)
 
-        # get page of address/ try to map it
-        map_address(addr, mode)
-
-'''
-- get address
-- find page address belongs to
-- try to map it
-- if it is, hit! - do nothing
-- if it's not:
-    - map it!
-        - enter into free space if available
-        - evict old one if necessary (using different algorithms)
-            - evict dirty (w)
-            - evict clean (r)
-
-
-
-'''
+    print '\nSTATS\n===='
+    print  replacement.capitalize()
+    print 'Number of Frames:\t {}'.format(MAX_FRAMES)
+    print 'Traces:\t {}'.format(traces)
+    print 'Hits:\t {}'.format(pt.hits)
+    print 'Faults:\t {}'.format(pt.faults)
+    print 'Writes:\t {}'.format(pt.disk_writes)
