9a1068a11ccd71788f3988c53faa57e5c23ee23d
saylorsd <Hambone#25>
2016-03-27 23:48:37 -0400

    added results page

--- script.py
+++ script.py
@@ -8,3 +8,4 @@ for method in methods:
     for frame_count in frame_counts:
         call = 'py E:/Projects/vm_simulator/vmsim.py -n {} -a {} -r {} ./traces/bzip.trace'.format(frame_count, method, refresh_rate)
         os.system(call)
+
